663f29c6a93fe9b8784b5f1cb0285cd0841103a7
ankush.singal <andysingal@gmail.com>
2023-03-22 14:35:56 +0530

    weather-app--- modified version

--- backend.py
+++ backend.py
@@ -1,5 +1,5 @@
 import requests
-API_KEY ="XXXXXX"
+API_KEY ="149d6cc109657cc69801c30ead0e47b6"
 def get_data(place,forecast_days=None,kind=None):
     url = f"https://api.openweathermap.org/data/2.5/forecast?q={place}&appid={API_KEY}"
     response = requests.get(url)
